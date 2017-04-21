a40d3dd085e4b3443996eba9135a5df93784c936
sue_charo <suehiro619@gmail.com>
2017-04-21 09:45:53 +0900

    cross_validationモジュールはなくなるから、model_selectionモジュールを用いると良い。

--- SupervisedLearningSVM.py
+++ SupervisedLearningSVM.py
@@ -6,7 +6,7 @@
 #    * 機械学習とは→ [コンピューターに"学習"させる](http://image.slidesharecdn.com/deepvision-141031063609-conversion-gate01/95/deep-learning-4-638.jpg) 
 #    * [サポートベクターマシン](http://image.slidesharecdn.com/shen2007-120927221259-phpapp01/95/proteinprotein-interaction-prediction-8-728.jpg) ・ [サポートベクターマシンとは](http://qiita.com/pesuchin/items/c55f40b69aa1aec2bd19)
 
-# In[1]:
+# In[12]:
 
 #（シャープ）以降の文字はプログラムに影響しません。
 # 図やグラフを図示するためのライブラリをインポートする。
@@ -15,7 +15,7 @@ import matplotlib.ticker as ticker
 get_ipython().magic('matplotlib inline')
 
 
-# In[2]:
+# In[13]:
 
 # 数値計算やデータフレーム操作に関するライブラリをインポートする
 import numpy as np
@@ -23,14 +23,14 @@ import pandas as pd
 from pandas.tools import plotting # 高度なプロットを行うツールのインポート
 
 
-# In[3]:
+# In[14]:
 
 # URL によるリソースへのアクセスを提供するライブラリをインポートする。
 # import urllib # Python 2 の場合
 import urllib.request # Python 3 の場合
 
 
-# In[4]:
+# In[15]:
 
 import sklearn #機械学習のライブラリ
 from sklearn.decomposition import PCA #主成分分析器
@@ -41,7 +41,7 @@ from sklearn.decomposition import PCA #主成分分析器
 
 # まず、データの読み込みを行います。データの読み込みについて不明点がある場合は<a href="ReadingCSV.ipynb" target="_blank">タブ区切りデータ、コンマ区切りデータ等の読み込み</a>を復習してください。
 
-# In[5]:
+# In[16]:
 
 # ウェブ上のリソースを指定する
 url = 'https://raw.githubusercontent.com/maskot1977/ipython_notebook/master/toydata/sbnote_dataJt.txt'
@@ -50,12 +50,12 @@ url = 'https://raw.githubusercontent.com/maskot1977/ipython_notebook/master/toyd
 urllib.request.urlretrieve(url, 'sbnote_dataJt.txt') # Python 3 の場合
 
 
-# In[6]:
+# In[17]:
 
 df = pd.read_csv('sbnote_dataJt.txt', sep='\t', index_col=0) # データの読み込み
 
 
-# In[7]:
+# In[18]:
 
 df
 
@@ -64,7 +64,7 @@ df
 
 # 次に、散布図行列を作成して変数間の関係を眺めます。散布図行列について不明点がある場合は<a href="ScatterMatrix.ipynb" target="_blank">散布図行列</a>を復習してください。
 
-# In[8]:
+# In[19]:
 
 # 散布図行列
 plotting.scatter_matrix(df[list(df.columns[:-1])], figsize=(10, 10)) 
@@ -74,7 +74,7 @@ plt.show()
 # 上図から、なんとなく２つのグループに分かれそうだということが分かりますね。そのグループは、真札・偽札の違いと対応しているのでしょうか？それを確認するため、カラーコードを用いて、"class"に基づいて散布図行列を色分けします。
 # * カラーコードが分からない人は右記リンク参照→ [カラーコード表](http://www5.plala.or.jp/vaio0630/hp/c_code.htm)・[カラーコード](http://www.finitojapan.com/cltable.html)
 
-# In[9]:
+# In[20]:
 
 # 色分けした散布図行列
 color_codes = ["#FF0000", "#0000FF", "#00FF00"]
@@ -94,13 +94,13 @@ plt.show()
 
 # それでは、もっと多くの変数を組み合わせればどうなるでしょうか？それを見るために、主成分分析をしてみましょう。主成分分析について不明点がある場合は<a href="PCA2017.ipynb" target="_blank">主成分分析</a>を復習してください。
 
-# In[10]:
+# In[ ]:
 
 # 行列の正規化
 dfs = df.iloc[:, :-1].apply(lambda x: (x-x.mean())/x.std(), axis=0).fillna(0)
 
 
-# In[11]:
+# In[ ]:
 
 #主成分分析の実行
 pca = PCA()
@@ -118,7 +118,7 @@ plt.show()
 
 # 上の主成分分析の結果を見ても、大きく２つのグループに分かれることがわかりました。これらのグループは真札と偽札とに対応してるのでしょうか？色分けをして確認してみましょう。
 
-# In[12]:
+# In[ ]:
 
 color_codes = ["#FF0000", "#0000FF", "#00FF00"]
 class_names = list(set(df.iloc[:, -1]))
@@ -152,7 +152,7 @@ plt.show()
 
 # 先ほど読み込んだデータを、上記の形式に合わせてPandasを用いて整形します。不明点がある場合は<a href="UsingNumpyAndPandas.ipynb" target="_blank">Numpy と Pandas を用いた演算</a>を復習してください。
 
-# In[13]:
+# In[ ]:
 
 feature_names = df.columns[:-1]
 target_names = list(set(df.iloc[:, -1]))
@@ -171,10 +171,10 @@ target = df.iloc[:, -1]
 # 交差検定 (cross-validation) とは → [交差検定](http://image.slidesharecdn.com/scikit-learn-141201042924-conversion-gate01/95/scikit-learn-15-638.jpg)
 # * 現在取得できているデータを「学習用セット」と「テスト用（評価用）セット」に分け、「学習用セット」だけを使って予測モデルを構築し、「テスト用セット」を使ってモデルの性能を評価します。
 
-# In[14]:
+# In[ ]:
 
-from sklearn import cross_validation as cv
-train_data, test_data, train_target, test_target = cv.train_test_split(data, target, test_size=0.5)
+from sklearn import model_selection as ms
+train_data, test_data, train_target, test_target = ms.train_test_split(data, target, test_size=0.5)
 
 
 # train_test_splitを用いると、dataとtargetの対応関係を保った上で、シャッフル（サンプルの順番をランダムに入れかえ）し、学習セットと評価セットに分割できます。
@@ -191,13 +191,13 @@ train_data, test_data, train_target, test_target = cv.train_test_split(data, tar
 
 # 念のため、分割したデータの中身を確認しましょう。train_dataとtest_dataで、順番がきちんとシャッフルされていて、どちらも意図した通りのデータサイズになっていること（たとえばtest_size=0.5としたならtrain_dataとtest_dataのサイズが1:1になっていること）を確認してください。
 
-# In[15]:
+# In[ ]:
 
 # 説明変数（学習用）
 train_data
 
 
-# In[16]:
+# In[ ]:
 
 # 説明変数（テスト用）
 test_data
@@ -208,13 +208,13 @@ test_data
 # * train_target と train_data のサンプルの数（N）と順序が揃っていること
 # * test_target と test_data のサンプルの数（N）と順序が揃っていること
 
-# In[17]:
+# In[ ]:
 
 # 目的変数（学習用）
 pd.DataFrame(train_target)
 
 
-# In[18]:
+# In[ ]:
 
 # 目的変数（テスト用）
 pd.DataFrame(test_target)
@@ -227,7 +227,7 @@ pd.DataFrame(test_target)
 # 
 # まずハイパーパラメータの設定を行います。ハイパーパラメータとは、簡単に言えば、機械学習を行う際に人間が予め設定しておかなければいけないパラメータのことです。
 
-# In[19]:
+# In[ ]:
 
 # 様々なパラメータ（ハイパーパラメータという）で学習し、予測性能の最も良いモデルを選択する。
 parameters = [
@@ -272,7 +272,7 @@ parameters = [
 # 
 # 以上の計３２通りのSVMで計算を行い、最も予測性能の高いものを選択することになります。本来はこのように非常に多数のハイパーパラメータを設定し最良の予測モデルを探索するのですが、たいへん長い計算時間を必要とすることがあります。時間の制約のため、本実習では、ハイパーパラメータの数を次のように減らして良いということにします。
 
-# In[20]:
+# In[ ]:
 
 # 様々なパラメータ（ハイパーパラメータという）で学習し、予測性能の最も良いモデルを選択する。
 parameters = [
@@ -289,7 +289,7 @@ parameters = [
 # 
 # 以上の計３通りのSVMで計算を行い、最も予測性能の高いものを選択することになります。では実際に計算してみましょう。
 
-# In[21]:
+# In[ ]:
 
 from sklearn import svm
 from sklearn.metrics import accuracy_score
@@ -314,7 +314,7 @@ print("elapsed_time:{0}".format(elapsed_time))
 
 # 予測精度はAccuracy_score:で表記された数値の通りですが、もう少し具体的に予測結果の中身を検討してみましょう。
 
-# In[22]:
+# In[ ]:
 
 # 予測結果と、本当の答えを比較する
 df = pd.DataFrame(columns=['test', 'pred'])
@@ -323,7 +323,7 @@ df['pred'] = pred_target # 予測された答え
 df.T
 
 
-# In[23]:
+# In[ ]:
 
 # 予測結果を本当の答えの比較を、混合行列（confusion matrix）で表現する
 from sklearn.metrics import confusion_matrix
@@ -333,7 +333,7 @@ pd.DataFrame(cm)
 
 # 対角線上にある数字が「正解」の個数ということになります。行列中の数字を眺めるのも良いですが、カラーマップに変換すれば見やすくなることもあります。やってみましょう。
 
-# In[24]:
+# In[ ]:
 
 # 混合行列（confusion matrix）をカラーマップで見やすくする
 plt.imshow(cm, interpolation='nearest', cmap=plt.cm.coolwarm)
@@ -355,7 +355,7 @@ plt.xlabel('Predicted label')
 
 # まず、データの読み込みを行います。データの読み込みについて不明点がある場合は<a href="ReadingCSV.ipynb" target="_blank">タブ区切りデータ、コンマ区切りデータ等の読み込み</a>を復習してください。
 
-# In[25]:
+# In[ ]:
 
 url = 'https://raw.githubusercontent.com/maskot1977/ipython_notebook/master/toydata/iris.txt'
 # 指定したURLからリソースをダウンロードし、名前をつける。
@@ -363,12 +363,12 @@ url = 'https://raw.githubusercontent.com/maskot1977/ipython_notebook/master/toyd
 urllib.request.urlretrieve(url, 'iris.txt') # Python 3 の場合
 
 
-# In[26]:
+# In[ ]:
 
 df = pd.read_csv('iris.txt', sep='\t', index_col=0) # データの読み込み
 
 
-# In[27]:
+# In[ ]:
 
 df
 
@@ -377,7 +377,7 @@ df
 
 # 次に、散布図行列を作成して変数間の関係を眺めます。散布図行列について不明点がある場合は<a href="ScatterMatrix.ipynb" target="_blank">散布図行列</a>を復習してください。
 
-# In[28]:
+# In[ ]:
 
 # 散布図行列
 plotting.scatter_matrix(df[list(df.columns[:-1])], figsize=(10, 10)) 
@@ -387,7 +387,7 @@ plt.show()
 # 上図から、なんとなく２つのグループ（大きなグループと小さなグループ）に分かれそうだということが分かりますね。そのグループは、種名の違いと対応しているのでしょうか？それを確認するため、カラーコードを用いて、"Species"に基づいて散布図行列を色分けします。
 # * カラーコードが分からない人は右記リンク参照→ [カラーコード表](http://www5.plala.or.jp/vaio0630/hp/c_code.htm)・[カラーコード](http://www.finitojapan.com/cltable.html)
 
-# In[29]:
+# In[ ]:
 
 # 色分けした散布図行列
 color_codes = ["#FF0000", "#0000FF", "#00FF00"]
@@ -401,13 +401,13 @@ plt.show()
 # 
 # Sepal.Length, Sepal.Width, Petal.Length, Petal.Widthのどの変数でも、１つだけ単独で用いるだけでもそこそこの分離性能は出せそうです。それでは、これらの変数を組み合わせればどうなるでしょうか？それを見るために、主成分分析をしてみましょう。主成分分析について不明点がある場合は<a href="PCA2017.ipynb" target="_blank">主成分分析</a>を復習してください。
 
-# In[30]:
+# In[ ]:
 
 # 行列の正規化
 dfs = df.iloc[:, :-1].apply(lambda x: (x-x.mean())/x.std(), axis=0).fillna(0)
 
 
-# In[31]:
+# In[ ]:
 
 #主成分分析の実行
 pca = PCA()
@@ -425,7 +425,7 @@ plt.show()
 
 # 上の主成分分析の結果を見ても、大きいグループと小さいグループに分かれることがわかりました。これらのグループと種名の関係はどうなっているでしょうか？色分けをして確認してみましょう。
 
-# In[32]:
+# In[ ]:
 
 color_codes = ["#FF0000", "#0000FF", "#00FF00"]
 class_names = list(set(df.iloc[:, -1]))
@@ -459,7 +459,7 @@ plt.show()
 
 # 先ほど読み込んだデータを、上記の形式に合わせてPandasを用いて整形します。不明点がある場合は<a href="UsingNumpyAndPandas.ipynb" target="_blank">Numpy と Pandas を用いた演算</a>を復習してください。
 
-# In[33]:
+# In[ ]:
 
 feature_names = df.columns[:-1]
 target_names = list(set(df.iloc[:, -1]))
@@ -478,7 +478,7 @@ target = df.iloc[:, -1]
 # 交差検定 (cross-validation) とは → [交差検定](http://image.slidesharecdn.com/scikit-learn-141201042924-conversion-gate01/95/scikit-learn-15-638.jpg)
 # * 現在取得できているデータを「学習用セット」と「テスト用（評価用）セット」に分け、「学習用セット」だけを使って予測モデルを構築し、「テスト用セット」を使ってモデルの性能を評価します。
 
-# In[34]:
+# In[ ]:
 
 from sklearn import cross_validation as cv
 train_data, test_data, train_target, test_target = cv.train_test_split(data, target, test_size=0.5)
@@ -498,13 +498,13 @@ train_data, test_data, train_target, test_target = cv.train_test_split(data, tar
 
 # 念のため、分割したデータの中身を確認しましょう。train_dataとtest_dataで、順番がきちんとシャッフルされていて、どちらも意図した通りのデータサイズになっていること（たとえばtest_size=0.5としたならtrain_dataとtest_dataのサイズが1:1になっていること）を確認してください。
 
-# In[35]:
+# In[ ]:
 
 # 説明変数（学習用）
 train_data
 
 
-# In[36]:
+# In[ ]:
 
 # 説明変数（テスト用）
 test_data
@@ -515,13 +515,13 @@ test_data
 # * train_target と train_data のサンプルの数（N）と順序が揃っていること
 # * test_target と test_data のサンプルの数（N）と順序が揃っていること
 
-# In[37]:
+# In[ ]:
 
 # 目的変数（学習用）
 pd.DataFrame(train_target)
 
 
-# In[38]:
+# In[ ]:
 
 # 目的変数（テスト用）
 pd.DataFrame(test_target)
@@ -534,7 +534,7 @@ pd.DataFrame(test_target)
 # 
 # まずハイパーパラメータの設定を行います。ハイパーパラメータとは、簡単に言えば、機械学習を行う際に人間が予め設定しておかなければいけないパラメータのことです。
 
-# In[39]:
+# In[ ]:
 
 parameters = [
     {'kernel': ['linear'], 'C': [1, 10, 100, 1000]},
@@ -578,7 +578,7 @@ parameters = [
 # 
 # 以上の計３２通りのSVMで計算を行い、最も予測性能の高いものを選択することになります。本来はこのように非常に多数のハイパーパラメータを設定し最良の予測モデルを探索するのですが、たいへん長い計算時間を必要とすることがあります。時間の制約のため、本実習では、ハイパーパラメータの数を次のように減らして良いということにします。
 
-# In[40]:
+# In[ ]:
 
 # 様々なパラメータ（ハイパーパラメータという）で学習し、予測性能の最も良いモデルを選択する。
 parameters = [
@@ -595,7 +595,7 @@ parameters = [
 # 
 # 以上の計３通りのSVMで計算を行い、最も予測性能の高いものを選択することになります。では実際に計算してみましょう。
 
-# In[41]:
+# In[ ]:
 
 from sklearn import svm
 from sklearn.metrics import accuracy_score
@@ -614,7 +614,7 @@ print("elapsed_time:{0}".format(elapsed_time))
 
 # 予測精度はAccuracy_score:で表記された数値の通りですが、もう少し具体的に予測結果の中身を検討してみましょう。
 
-# In[42]:
+# In[ ]:
 
 df = pd.DataFrame(columns=['test', 'pred'])
 df['test'] = test_target
@@ -622,7 +622,7 @@ df['pred'] = pred_target
 df.T
 
 
-# In[43]:
+# In[ ]:
 
 # 予測結果を本当の答えの比較を、混合行列（confusion matrix）で表現する
 from sklearn.metrics import confusion_matrix
@@ -632,7 +632,7 @@ pd.DataFrame(cm)
 
 # 対角線上にある数字が「正解」の個数ということになります。行列中の数字を眺めるのも良いですが、カラーマップに変換すれば見やすくなることもあります。やってみましょう。
 
-# In[44]:
+# In[ ]:
 
 # 混合行列（confusion matrix）をカラーマップで見やすくする
 plt.imshow(cm, interpolation='nearest', cmap=plt.cm.coolwarm)
